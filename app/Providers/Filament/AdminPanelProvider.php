400bd7dcac376b2135e20b1edef292ef5a371b1b
IvoDS <43913533+IvoDS@users.noreply.github.com>
2026-04-19 14:16:05 +0200

    Fix mobile UI.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,8 +28,9 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->maxContentWidth('full')
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => '#6a4c94',
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
